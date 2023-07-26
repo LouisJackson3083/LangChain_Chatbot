dadfb5cc31dcabdae094dae3713e371e866eac73
Louis Jackson <louis.jackson@adarga.ai>
2023-07-26 16:29:28 +0100

    Overhaul chatbot with panel serve featuring new, easier to interact with UI and chat with data functionality. TODO: reimplement memory options and improve prompt engineering

--- README.md
+++ README.md
@@ -1,12 +1,17 @@
 # LangChain_Chatbot
 ## Environment setup
 First create a virtual python environment using your preferred method, and execute:
-`pip install -r requirements.txt`
+`
+python3 -m venv venv
+source venv/bin/activate
+python3 -m pip install --upgrade pip
+pip3 install -r requirements.txt
+`
 ## Running the Chatbot
-
 To run the chatbot, please run the following command in a terminal (vs-code terminal does not work)
-`python3 chatbot.py <API_KEY>`
+`panel serve chatbot_v2.py --autoreload --show --args <API_KEY>`
 You must have an OpenAI api key which you can get here: https://platform.openai.com/account/api-keys
+
 ## Potential Errors
 I you get an error like this:
 `Retrying langchain.chat_models.openai.ChatOpenAI.completion_with_retry.<locals>._completion_with_retry in 1.0 seconds as it raised RateLimitError: You exceeded your current quota, please check your plan and billing details..`
--- chatbot.py
+++ chatbot.py
@@ -1,120 +1,184 @@
-import os, sys
+import os, openai, sys
+import panel as pn
+import param
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.text_splitter import CharacterTextSplitter, RecursiveCharacterTextSplitter
+from langchain.vectorstores import DocArrayInMemorySearch
+from langchain.chains import RetrievalQA,  ConversationalRetrievalChain, ConversationChain, LLMChain
+from langchain.memory import ConversationBufferMemory
 from langchain.chat_models import ChatOpenAI
-from langchain.chains import ConversationChain
-from langchain.memory import ConversationBufferMemory, ConversationBufferWindowMemory, ConversationTokenBufferMemory
-from utils import clear_terminal, get_user_input, display_conversation
-from termcolor import colored
+from langchain.document_loaders import TextLoader, PyPDFLoader
+from langchain.prompts import ChatPromptTemplate
 
-class Chatbot():
-    
-    def __init__(self, api_key):
-        self.api_key = api_key
-        self.verbose = False
-        self.temperature = 0.0
-        self.memory_options = ["ConversationalBufferMemory", "ConversationBufferWindowMemory", "ConversationTokenBufferMemory"]
-        self.memory_current = self.memory_options[0]
 
-        self.llm = ChatOpenAI(openai_api_key=api_key, temperature=0.0)
+def load_db(file, chain_type, k):
+    # load documents
+    loader = PyPDFLoader(file)
+    documents = loader.load()
+    # split documents
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=150)
+    docs = text_splitter.split_documents(documents)
+    # define embedding
+    embeddings = OpenAIEmbeddings()
+    # create vector database from data
+    db = DocArrayInMemorySearch.from_documents(docs, embeddings)
+    # define retriever
+    retriever = db.as_retriever(search_type="similarity", search_kwargs={"k": k})
+    # create a ChatBot chain. Memory is managed externally.
+    qa = ConversationalRetrievalChain.from_llm(
+        llm=ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0), 
+        chain_type=chain_type, 
+        retriever=retriever, 
+        return_source_documents=True,
+        return_generated_question=True,
+    )
+    return qa 
+
+
+class ChatBot():
+    def __init__(self):
+        self.loaded_file = None
+        self.qa = None
+        self.panels = []
+        self.chat_history = []
+        self.llm=ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
         self.memory = ConversationBufferMemory()
-        self.conversation = ConversationChain(
+        self.conversation_chain = ConversationChain(
             llm = self.llm,
             memory = self.memory,
-            verbose = self.verbose
+            verbose = False
         )
+        self.translator_segmenter = LLMChain(llm=self.llm, prompt=prompt_segment)
+        self.translator_translate = LLMChain(llm=self.llm, prompt=prompt_translate)
 
-    def change_settings(self):
-        print(colored(f"What setting would you like to change?", "green") + colored(f"\n1. Verbosity: {self.verbose}\n2. Temperature: {self.temperature}\n3. Memory: {self.memory_current}", "yellow"))
-        setting_choice = -1
-        while (setting_choice < 0 or setting_choice > 3):
-            try:
-                setting_choice = int(input(colored(f"Enter the number of the setting: ","green")))
-            except ValueError:
-                print(colored(f"Invalid input", "red"))
+    def call_load_db(self, count):
+        if count == 0 or file_input.value is None:  # init or no file specified :
+            return pn.pane.Markdown(f"Loaded File: {self.loaded_file}")
+        else:
+            file_input.save("temp.pdf")  # local copy
+            self.loaded_file = file_input.filename
+            button_load.button_style="outline"
+            self.qa = load_db("temp.pdf", "stuff", 4)
+            button_load.button_style="solid"
+        self.clr_history()
+        return pn.pane.Markdown(f"Loaded File: {self.loaded_file}")
 
-        if (setting_choice == 1):
-            self.change_verbose()
-        elif (setting_choice == 2):
-            self.change_temperature()
-        elif (setting_choice == 3):
-            self.change_memory()
+    def conversation(self, query):
+        if not query: # if there are no queries
+            return pn.WidgetBox(pn.Row('User:', pn.pane.Markdown("", width=600)), scroll=True)
+        result = self.conversation_chain.predict(input=query) # if we have a query get a response from the ChatBot
+        self.chat_history.append([(query, result)]) # add it to the chat_history
+        self.panels.extend([ # display it
+            pn.Row('User:', pn.pane.Markdown(query, width=600, styles={'background-color': '#FFFFDD'})),
+            pn.Row('ChatBot:', pn.pane.Markdown(result, width=600, styles={'background-color': '#DDFFDD'}))
+        ])
+        panel_conversation_input.value = ''  # clears loading indicator when cleared
+        return pn.WidgetBox(*self.panels, scroll=True)
+    
+    def data_conversation(self, query):
+        if not query:
+            return pn.WidgetBox(pn.Row('User:', pn.pane.Markdown("", width=600)), scroll=True)
+        # Call our qa on the loaded document
+        result = self.qa({"question": query, "chat_history": self.chat_history})
+        # append the results to the chat history
+        self.chat_history.append([(query, result["answer"])])
+        self.db_query = result["generated_question"]
+        self.db_response = result["source_documents"]
+        self.answer = result['answer'] 
+        self.panels.extend([ # display it
+            pn.Row('User:', pn.pane.Markdown(query, width=600, styles={'background-color': '#FFFFDD'})),
+            pn.Row('ChatBot:', pn.pane.Markdown(result, width=600, styles={'background-color': '#DDFFDD'}))
+        ])
+        panel_data_conversation_input.value = ''  #clears loading indicator when cleared
+        return pn.WidgetBox(*self.panels,scroll=True)
+    
+    def translator(self, query):
+        if not query: # if there are no queries
+            return pn.WidgetBox(pn.Row('User:', pn.pane.Markdown("", width=600)), scroll=True)
+        result_segment = self.translator_segmenter({'text':query})['text']
+        result_translate = self.translator_translate({'text':query})['text']
+        self.panels.extend([ # display it
+            pn.Row('User:', pn.pane.Markdown(query, width=600, styles={'background-color': '#FFFFDD'})),
+            pn.Row('AI Translation:', pn.pane.Markdown(result_translate, width=600, styles={'background-color': '#DDFFDD'})),
+            pn.Row('AI Segmentation:', pn.pane.Markdown(result_segment, width=600, styles={'background-color': '#DDFFDD'}))
+        ])
+        panel_translator_input.value = ''  # clears loading indicator when cleared
+        return pn.WidgetBox(*self.panels, scroll=True)
 
-    def change_verbose(self):
-        self.verbose = not self.verbose
-        self.conversation.verbose = self.verbose
-        print(colored(f"Verbosity has been changed!", "yellow"))
+    def clr_history(self,count=0):
+        self.chat_history = []
+        return 
 
-    def change_temperature(self):
-        new_temperature = -1
-        while (new_temperature < 0 or new_temperature > 1):
-            try:
-                new_temperature = float(input(colored(f"Old temperature: {self.temperature} , Enter new temperature: ","green")))
-            except ValueError:
-                print(colored(f"Invalid input", "red"))
-        self.temperature = new_temperature
-        self.llm.temperature = self.temperature
-        self.conversation.llm = self.llm
-        print(colored(f"Temperature has been changed!", "yellow"))
+# Load in the open api key as an environment variable
+os.environ['OPENAI_API_KEY'] = str(sys.argv[1])
+openai.api_key = os.environ['OPENAI_API_KEY']
 
-    def change_memory(self):
-        print(colored(f"The memory method the chatbot is currently using is: {self.memory_current}\nYour options are:", "green"))
-        for index, memory_method in enumerate(self.memory_options):
-            print(colored(str(index+1) + ". " + str(memory_method), "yellow"))
-        memory_choice = -1
-        while (memory_choice < 0 or memory_choice > 2):
-            try:
-                memory_choice = int(input(colored(f"Enter the number of the new memory: ","green")))
-            except ValueError:
-                print(colored(f"Invalid input", "red"))
+# prompt_segment = ChatPromptTemplate.from_template(
+#     """
+#     For each identified languages in this text: {text}
+#     please return the segmented texts that are of that language in this format:
+#     '<language_1>: <text_1> \n
+#     <language_1>: <text_2> \n
+#     <language_n>: <text_n> \n'
+#     """
+# )
 
-        
-        self.memory_current = self.memory_options[memory_choice-1]
-        if memory_choice == 1:
-            self.memory = ConversationBufferMemory()
-        elif memory_choice == 2:
-            k_choice = -1
-            while (k_choice < 0):
-                try:
-                    k_choice = int(input(colored(f"Enter the k number for the window buffer (default is 5): ","green")))
-                except ValueError:
-                    print(colored(f"Invalid input", "red"))
-            self.memory = ConversationBufferWindowMemory(k=k_choice)
-        # elif memory_choice == 3:
-        #     token_choice = -1
-        #     while (token_choice < 0):
-        #         try:
-        #             token_choice = int(input(colored(f"Enter the max token limit for the memory (default is 30): ","green")))
-        #         except ValueError:
-        #             print(colored(f"Invalid input", "red"))
-        #     self.memory = ConversationTokenBufferMemory(llm=self.llm, max_token_limit=token_choice)
+prompt_segment = ChatPromptTemplate.from_template(
+    """
+    ## Instruction ##
+    Identify each language in the text below, and segment the text by language:
+    {text}
+    """
+)
 
-        self.conversation = ConversationChain(
-            llm = self.llm,
-            memory = self.memory,
-            verbose = self.verbose
-        )
+prompt_translate = ChatPromptTemplate.from_template(
+    """
+    ## Instruction ##
+    Translate the text below to English:
+    {text}
+    """
+)
 
+cb = ChatBot()
 
-def main_chat_loop(chatbot):
-    clear_terminal()
+# Conversation panels
+panel_conversation_input = pn.widgets.TextInput(placeholder='Enter text here...')
+panel_conversation = pn.bind(cb.conversation, panel_conversation_input) # this binds the input box to the ChatBot's conversation function (where the llm is called)
 
-    user_input = get_user_input()
-    while (user_input == "settings"):
-        chatbot.change_settings()
-        user_input = get_user_input()
-        clear_terminal()
+# Translator panels
+panel_translator_input = pn.widgets.TextInput(placeholder='Enter text to be translated here...')
+panel_translator = pn.bind(cb.translator, panel_translator_input) # this binds the input box to the ChatBot's conversation function (where the llm is called)
 
-    while user_input != "exit":
-        clear_terminal()
-        print(colored("AI is thinking...", "green"))
-        chatbot.conversation.predict(input=user_input)
-        display_conversation(memory_buffer=chatbot.memory.buffer)
-        user_input = get_user_input()
-        while (user_input == "settings"):
-            chatbot.change_settings()
-            user_input = get_user_input()
-            clear_terminal()
+# Data conversation panels
+file_input = pn.widgets.FileInput(accept='.pdf')
+button_load = pn.widgets.Button(name="Load DB", button_type='primary')
+bound_button_load = pn.bind(cb.call_load_db, button_load.param.clicks)
+panel_data_conversation_input = pn.widgets.TextInput(placeholder='Enter text here...')
+panel_data_conversation = pn.bind(cb.data_conversation, panel_data_conversation_input) # this binds the input box to the ChatBot's conversation function (where the llm is called)
+         
+tab_conversation = pn.Column( # this tab is the ChatBot tab
+    pn.Row(panel_conversation_input), # create the input box
+    pn.layout.Divider(),
+    pn.panel(panel_conversation, loading_indicator=True, height=300), # display the conversation
+    pn.layout.Divider(),
+)
+tab_data_conversation = pn.Column( # this tab is the ChatBot tab
+    pn.Row(file_input, button_load, bound_button_load),
+    pn.Row(panel_data_conversation_input), # create the input box
+    pn.layout.Divider(),
+    pn.panel(panel_data_conversation, loading_indicator=True, height=300), # display the conversation
+    pn.layout.Divider(),
+)
+tab_translator = pn.Column( # this tab is the ChatBot tab
+    pn.Row(panel_translator_input), # create the input box
+    pn.layout.Divider(),
+    pn.panel(panel_translator, loading_indicator=True, height=300), # display the conversation
+    pn.layout.Divider(),
+)
+panel_master = pn.Column( # this panel creates the title, "logo", and is the parent of all the individual tab panels
+    pn.Row(pn.pane.PNG('./img/langchain.png', width=400)),
+    pn.Row(pn.pane.Markdown('# ChatBot Implementation')),
+    pn.Tabs(('Chat',tab_conversation), ('Chat with Data',tab_data_conversation), ('Translate',tab_translator), )
+)
 
-if __name__ == "__main__":
-    api_key = str(sys.argv[1])
-    chatbot = Chatbot(api_key=api_key)
-    main_chat_loop(chatbot=chatbot)
+panel_master.servable()
